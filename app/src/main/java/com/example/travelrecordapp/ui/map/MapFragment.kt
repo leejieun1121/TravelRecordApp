27a07db496d4a097d195554b553a1ffddc8d3e75
이지은 <lje4648@naver.com>
2021-06-03 23:55:56 +0900

    위치 권한 없을때 얻어오기 추가

--- app/src/main/java/com/example/travelrecordapp/ui/map/MapFragment.kt
+++ app/src/main/java/com/example/travelrecordapp/ui/map/MapFragment.kt
@@ -1,16 +1,26 @@
 package com.example.travelrecordapp.ui.map
 
 import android.Manifest
+import android.Manifest.permission.ACCESS_COARSE_LOCATION
+import android.Manifest.permission.ACCESS_FINE_LOCATION
+import android.annotation.SuppressLint
 import android.content.Context.LOCATION_SERVICE
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
+import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
@@ -21,6 +31,7 @@ import com.google.android.gms.maps.*
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.material.snackbar.Snackbar
 
 
 class MapFragment : Fragment(),OnMapReadyCallback{
@@ -31,6 +42,7 @@ class MapFragment : Fragment(),OnMapReadyCallback{
     private lateinit var mLocationListener: LocationListener
     private lateinit var mMap: GoogleMap
 
+    private lateinit var layout : View
     private var lat =0.0
     private var lng =0.0
 
@@ -47,25 +59,19 @@ class MapFragment : Fragment(),OnMapReadyCallback{
         mapFragment.getMapAsync(this)
         return binding.root
     }
-
     override fun onMapReady(googleMap: GoogleMap) {
         //TODO 퍼미션 설정
         mMap = googleMap
-
+        layout = binding.layout
         //맨처음 구글 지도 load 할때 현재 위치 찾는 시간이 좀 걸리기 때문에 설정을 안해주면 아프리카에 있음 ㅎ
+        permissionCheck()
         setDefaultLocation()
         getCurrentLocation()
-        permissionCheck()
     }
 
     private fun permissionCheck(){
-        if (ContextCompat.checkSelfPermission(
-                requireContext(),
-                Manifest.permission.ACCESS_COARSE_LOCATION
-            ) == PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(
-                requireContext(),
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ) == PackageManager.PERMISSION_GRANTED
+        if (ContextCompat.checkSelfPermission(requireContext(), ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED &&
+            ContextCompat.checkSelfPermission(requireContext(), ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
         ) {
             mLocationManager.requestLocationUpdates(
                 LocationManager.GPS_PROVIDER,
@@ -73,8 +79,39 @@ class MapFragment : Fragment(),OnMapReadyCallback{
                 30f,
                 mLocationListener
             )
+        } else{
+            requestMultiplePermissions.launch(
+                arrayOf(ACCESS_COARSE_LOCATION, ACCESS_FINE_LOCATION))
+
         }
     }
+
+    @SuppressLint("MissingPermission")
+    private val requestMultiplePermissions =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+            permissions.entries.forEach {
+                Log.d("tag", "${it.key} = ${it.value}")
+            }
+            if (permissions[ACCESS_COARSE_LOCATION] == true && permissions[ACCESS_FINE_LOCATION] == true) {
+                mLocationManager.requestLocationUpdates(
+                    LocationManager.GPS_PROVIDER,
+                    3000L,
+                    30f,
+                    mLocationListener
+                )
+
+            } else {
+                val snackBar = Snackbar.make(layout, "권한이 없습니다. 해당 권한을 얻기 위해 설정으로 이동합니다.", Snackbar.LENGTH_INDEFINITE)
+                snackBar.setAction("확인") {
+                    val intent = Intent()
+                    intent.action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+                    val uri = Uri.fromParts("package", "com.example.travelrecordapp.ui.map", null)
+                    intent.data = uri
+                    startActivity(intent)
+                }
+                snackBar.show()
+            }
+        }
 
     private fun getCurrentLocation(){
         mLocationManager = requireContext().getSystemService(LOCATION_SERVICE) as LocationManager
